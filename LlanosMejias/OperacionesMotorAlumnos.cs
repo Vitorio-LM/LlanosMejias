343b104705126a05ac7d74f5eac5e5a8166e7061
victor <victorllanosmejias@gmail.com>
2026-07-03 23:18:11 -0400

    Carga a bd

--- LlanosMejias/OperacionesMotorAlumnos.cs
+++ LlanosMejias/OperacionesMotorAlumnos.cs
@@ -1,10 +1,10 @@
-﻿using LlanosMejias.Configuracion;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using LlanosMejias.Configuracion;
 
 namespace LlanosMejias
 {
